2f22f9a72a7088d9699d72e409d1214a37df6b8b
Peter Brown <peter.brown@plannuh.com>
2022-11-09 22:53:56 -0500

    Change blocks to reference index not stateID; updated example.js

--- example.js
+++ example.js
@@ -2,14 +2,14 @@ const fs = require('fs').promises
 const { Schematic } = require('prismarine-schematic')
 
 async function main () {
-  //const schematic = await Schematic.read(await fs.readFile('test/schematics/smallhouse1.schem'))
+  const schematic = await Schematic.read(await fs.readFile('test/schematics/smallhouse1.schem'))
 
-  //await fs.writeFile('test.schem', await schematic.write())
+  await fs.writeFile('test.schem', await schematic.write())
 
-  //const schematic2 = await Schematic.read(await fs.readFile('test.schem'))
+  const schematic2 = await Schematic.read(await fs.readFile('test.schem'))
 
-  //console.log("____________")
-  //console.log(schematic2)
+  console.log("____________")
+  console.log(schematic2)
 
   const schematic3 = await Schematic.read(await fs.readFile('test/schematics/Zombie_Box.litematic'));
   
